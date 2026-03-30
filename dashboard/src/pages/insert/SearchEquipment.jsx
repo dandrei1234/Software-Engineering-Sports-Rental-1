06f3562b8b9b71294d03292653c98dc56b2a8826
@dandrei1234dandrei1234 <andreiraymundo29@gmail.com>
2026-03-30 20:04:30 +0800

    Added column and table data for search equipment

--- dashboard/src/pages/insert/SearchEquipment.jsx
+++ dashboard/src/pages/insert/SearchEquipment.jsx
@@ -4,7 +4,7 @@ import {
   Box, Card, CardContent,
   TextField, Button, MenuItem,
   Table, TableBody, TableCell,
-  TableRow
+  TableRow, TableHead, Stack
 } from '@mui/material';
 
 
@@ -153,34 +153,49 @@ const SearchEquipment = () => {
 
               {searchedRows.length > 0?
               <Table>
+                <TableHead>
+                    <TableRow>
+                    <TableCell>Equipment Name</TableCell>
+                    <TableCell align="center">Category</TableCell>
+                    <TableCell align="center">Condition</TableCell>
+                    <TableCell align="center">Borrow Status</TableCell>
+                    <TableCell align="center">Dates</TableCell>
+                    <TableCell align="center">Action</TableCell>
+                    </TableRow>
+                </TableHead>
                 <TableBody>
                     {searchedRows.map((row, index) => (
                         <TableRow key={index}>
                             <TableCell>{row.equipment_name}</TableCell>
+                            <TableCell align="center">{row.category_name}</TableCell>
+                            <TableCell align="center">{row.condition_status}</TableCell>
+                            <TableCell align="center">{row.borrow_status}</TableCell>
+                            <TableCell sx={{fontSize: '12px', minWidth: '150px'}}>
+                                {row.due_date? ("Due: ", row.due_date, <br/>) : ""}
+                                {row.return_date? "Return: " + row.return_date : ""}
+                                {row.request_date? "Request: " + row.request_date : ""}
+                            </TableCell>
                             <TableCell
-                                
-                                sx={{
-                                    display: "flex",
-                                    gap: "12px"
-                                }}>
-
-                                <Button variant="contained" color="primary"
-                                    sx={{
-                                        backgroundColor: "#5496e0",
-                                        color: "white",
-                                        '&:hover': {
-                                            backgroundColor: "#2777b2"
-                                        }
-                                    }}>Edit</Button>
-
-                                <Button variant="contained"
-                                    sx={{
-                                        backgroundColor: "#e09090",
-                                        color: "white",
-                                        '&:hover': {
-                                            backgroundColor: "#a75454"
-                                        }
-                                    }}>Delete</Button>
+                                align="center">
+                                <Stack direction='row' spacing='5px'>
+                                  <Button variant="contained" color="primary"
+                                      sx={{
+                                          backgroundColor: "#5496e0",
+                                          color: "white",
+                                          '&:hover': {
+                                              backgroundColor: "#2777b2"
+                                          }
+                                      }}>Approve</Button>
+                                      
+                                  <Button variant="contained" color="primary"
+                                      sx={{
+                                          backgroundColor: "#e07785",
+                                          color: "white",
+                                          '&:hover': {
+                                              backgroundColor: "#b34553"
+                                          }
+                                      }}>Reject</Button>
+                                </Stack>
                             </TableCell>
                         </TableRow>
                         ))}
